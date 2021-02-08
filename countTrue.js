fa9999c85cbe8ec08165ba4eb7acb93834c2e045
caldric <cvaldric@gmail.com>
2021-02-08 13:07:59 -0500

    Write countTrue function in JavaScript
    Goal: write a function that counts the number of occurrences of the
    boolean true on an array.

---
+++ countTrue.js
@@ -0,0 +1,11 @@
+/**
+ * Counts the number of occurrences of the boolean true on an array.
+ */
+const countTrue = (arr) => arr.filter((elem) => elem === true).length;
+
+console.log(countTrue([true, false, true, true, false])); // returns 3
+console.log(countTrue([false, false])); // returns 0
+console.log(countTrue([true])); // returns 1
+console.log(countTrue([])); // returns 0
+console.log(countTrue('is this the real life?'.split(' '))); // returns 0
+console.log(countTrue('is this just fantasy?'.split(' '))); // returns 0
